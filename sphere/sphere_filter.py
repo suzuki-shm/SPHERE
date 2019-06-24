[BUG] Fix bug in variance filter

sphere/sphere_filter.py
@@ -72,16 +72,20 @@ def main(args, logger):
         cl = compress_length(df["depth"].size, s=args["s"], w=args["w"])
         genome_name = df["genome"].unique()[0]
         location = np.arange(1, cl + 1, 1).astype(int)
-        c_depth = moving_filter(df["depth"], s=args["s"], w=args["w"], ftype=args["t"])
+        c_depth = moving_filter(df["depth"],
+                                s=args["s"],
+                                w=args["w"],
+                                ftype=args["t"])
         f_df = pd.DataFrame({"location": location, "depth": c_depth})
         f_df["genome"] = genome_name
         f_df = f_df[["genome", "location", "depth"]]
     elif args["t"] == "variance":
-        p = df["location"].max()
+        p = 1 / df["location"].max()
         n = df["depth"].sum()
         m = df["depth"].mean()
+        # Compute the variance based on binomial distribution
         v = n * p * (1-p)
-        index = df.query("depth - {0} > {0}*{1}".format(m, args["r"], v)).index
+        index = df.query("abs(depth-{0})>{1}*{2}".format(m, args["r"], v)).index
         df.loc[index, "depth"] = args["m"]
         f_df = df
     elif args["t"] == "percentile":